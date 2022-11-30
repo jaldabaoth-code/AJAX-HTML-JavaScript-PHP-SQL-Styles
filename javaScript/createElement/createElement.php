ed74cb1e30a53ba097bc60e8b59422ef6339571d
jaldabaoth-code <grialazurabi@gmail.com>
2022-12-01 00:24:30 +0100

    save

--- javaScript/createElement/createElement.php
+++ javaScript/createElement/createElement.php
@@ -1,20 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <?php
-            /* head included */
-            include '../includes/head.html';
-        ?>
-        <link rel="stylesheet" href="../assets/styles/style.css">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" type="image/png" href="#" />
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+        <link rel="stylesheet" href="../../assets/styles/navbar.css">
+        <link rel="stylesheet" href="../../assets/styles/style.css">
         <link rel="stylesheet" href="./src/styles.css" />
         <title>Create element JS</title>
     </head>
     <?php
-        /* navbar included */
-        include '../includes/navbar.php';
+        /* Navbar included */
+        include '../../includes/navbar.php';
+        include '../includes/bottomNavbar.php';
     ?>
     <body>
-        <h1 class="mt-5">Create element JS</h1>
+        <h1 class="mt-5">JS Create element - Quest</h1>
         <h2>Adopt A Cat!</h2>
         <div class="cards">
             <div class="card">
--- javaScript/guessNumber/guessNumber.php
+++ javaScript/guessNumber/guessNumber.php
@@ -6,14 +6,14 @@
             include '../includes/head.html';
         ?>
         <link rel="stylesheet" href="../assets/styles/style.css">
-        <title>Guess number JS</title>
+        <title>JavaScipt Guess A Number</title>
     </head>
     <?php
         /* navbar included */
         include '../includes/navbar.php';
     ?>
     <body>
-        <h2>Guess Number JS</h2>
+        <h2>JavaScipt Guess A Number - Quest</h2>
         <script src="script.js"></script>
     </body>
 </html>
